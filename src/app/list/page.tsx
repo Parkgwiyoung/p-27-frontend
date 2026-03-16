86fd0c971ffe2788edbaef9d9646514d60c62cb2
Parkgwiyoung <pgwi11@naver.com>
2026-03-16 15:47:47 +0900

    13-2

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,29 +1,32 @@
-"use client";
+"use client"
 
 import { useEffect, useState } from "react";
 
+interface Post {
+    id: number,
+    title: string
+}
+
 export default function Home() {
-  const [posts, setPosts] = useState<{ id: number; title: string }[]>([]);
 
-  useEffect(() => {
-    fetch("http://localhost:8080/api/v1/posts")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setPosts(data);
-      });
-  }, []);
+    const [posts, setPosts] = useState<Post[]>([]);
 
-  return (
-    <div className="flex flex-col gap-9">
-      <h1>글 목록</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            {post.id}. {post.title}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    useEffect(() => {
+        fetch("http://localhost:8080/api/v1/posts")
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                setPosts(data);
+            });
+    }, []);
+
+    return (
+        <ul>
+            {posts.map((post) => (
+                <li key={post.id} className="p-2">
+                    {post.id}. {post.title}
+                </li>
+            ))}
+        </ul>
+    );
 }
