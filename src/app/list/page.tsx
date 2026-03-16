640ede8990b18b64cbfa53d103b4618062b77d85
Parkgwiyoung <pgwi11@naver.com>
2026-03-16 15:35:46 +0900

    13

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,3 +1,29 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Home() {
-    return <div>여기는 list 페이지입니다.</div>;
-  }
+  const [posts, setPosts] = useState<{ id: number; title: string }[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/v1/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      });
+  }, []);
+
+  return (
+    <div className="flex flex-col gap-9">
+      <h1>글 목록</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            {post.id}. {post.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
