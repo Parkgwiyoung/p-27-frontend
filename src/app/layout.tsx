64a063f530b2620955abb70f59add9479753d203
Parkgwiyoung <pgwi11@naver.com>
2026-03-16 15:05:54 +0900

    12

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased !bg-red-300 border-2 border-black min-h-screen
-        flex flex-col
+        className={`${geistSans.variable} ${geistMono.variable} 
+        antialiased min-h-screen flex flex-col 
         `}
       >
         <header>
@@ -36,7 +36,9 @@ export default function RootLayout({
             <Link href="/list">목록</Link>
           </nav>
         </header>
-        <main className="flex-grow">{children}</main>
+        <main className="flex-grow flex justify-center items-center">
+          {children}
+        </main>
         <footer>푸터</footer>
       </body>
     </html>
